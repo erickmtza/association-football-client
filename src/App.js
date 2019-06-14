7b8497bea4bbfe5bfa62b1e7d3ee085d3069a250
Erick Martinez <erick.martinez2013@gmail.com>
2019-06-13 21:26:20 -0500

    Added header links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <main className='App'>
+      <header className='app_header'>
+        <Link
+            to='/login'>
+            Log in
+          </Link>
+          <Link
+            to='/register'>
+            Register
+          </Link>
+      </header>
     </main>
   );
 }
