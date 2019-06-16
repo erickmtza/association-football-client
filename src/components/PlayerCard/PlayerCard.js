f5405755e14d889b8f471df059900a50db464c1c
Erick Martinez <erick.martinez2013@gmail.com>
2019-06-15 20:08:31 -0500

    created component for individual player cards

---
+++ src/components/PlayerCard/PlayerCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function PlayerCard({ player, id }) {
+    console.log(id)
+    return (
+        <li key={id}>
+            <img src={player.img} alt={player.name}/>
+            <p>{Math.ceil((Number(player.att) + Number(player.def) + Number(player.spd)) / 3)} AVG.</p>
+            <p>Name: {player.name}</p>
+            <p>Pos: {player.pos}</p>
+            <p>ATT: {player.att}</p>
+            <p>DEF: {player.def}</p>
+            <p>SPD: {player.spd}</p>
+        </li>
+    )
+}
