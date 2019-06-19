a2fe7a448735f9de11f1eb3bbbbd4fe377bbca76
Erick Martinez <erick.martinez2013@gmail.com>
2019-06-18 19:49:31 -0500

    Api service to get data of players endpoint

---
+++ src/services/players-api-service.js
@@ -0,0 +1,48 @@
+import TokenService from '../services/token-service'
+import config from '../config'
+
+const ArticleApiService = {
+  getPlayers() {
+    return fetch(`${config.API_ENDPOINT}/players`, {
+      headers: {
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  getPlayers(player_id) {
+    return fetch(`${config.API_ENDPOINT}/players/${player_id}`, {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  postPlayer(player_id, text) {
+    return fetch(`${config.API_ENDPOINT}/players`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+      body: JSON.stringify({
+        user_id: player_id,
+        text,
+      }),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  }
+}
+
+export default ArticleApiService
