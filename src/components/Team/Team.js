ffbed4ef0db2db21af689b8424c5097fa36b5f25
Erick Martinez <erick.martinez2013@gmail.com>
2019-06-18 20:48:21 -0500

    Removed consolelogs

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -5,20 +5,16 @@ import PlayerCard from '../../components/PlayerCard/PlayerCard'
 export default function Team({players, filterPlayersPos}) {
 
     const filter = () => {
-        console.log(filterPlayersPos)
         if(filterPlayersPos === "All" || !filterPlayersPos) {
-            return players.map((player, i) => (
+            return players.map((player) => (
                 <PlayerCard
                     player={player}
-                    id={i}
                 />
             ))
         } else {
-            console.log(filterPlayersPos)
             return players.filter((player) => player.pos === filterPlayersPos).map((player, i) => (
                 <PlayerCard
                     player={player}
-                    id={i}
                 />
             ))
         }
