fa897beb73dc3543353f2acd6ea1b6d133bcb69b
Erick Martinez <erick.martinez2013@gmail.com>
2019-06-21 19:23:41 -0500

    Added styling to landing page

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -5,17 +5,19 @@ export default function LandingPage() {
     return (
         <section>
             <header className="hero">
-                <h1><span>Association</span> Football</h1>
+                <h1>
+                    <span className="association">Association</span>
+                    <span>Football</span>
+                </h1>
             </header>
            
-           <div className="info-land">
-               <h2>ASSEMBLE YOUR TEAM</h2>
-               <p>Pick the players you want from the big leagues for the ultimate team.</p>
+           <div className="info-land assemble">
+               <h2>Assemble Your Team</h2>
+               <p>Pick the players you want from the big leagues for the ultimate team. Build the dream, Coach!</p>
             </div>
-            <div className="info-land">
+            <div className="info-land coaching">
                 <h2>Coaching a Team?</h2>
-                <p>Use our dashboard to keep track of your team. Professionally.</p>
-                <p>Img placeholder</p>
+                <p>Use our dashboard to keep track of your team's player stats and positions. Professionally.</p>
             </div> 
         </section>
     )
