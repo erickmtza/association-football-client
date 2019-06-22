10db612af1b258446431877e3815324e974f1d7c
Erick Martinez <erick.martinez2013@gmail.com>
2019-06-21 19:22:19 -0500

    Changed content to use li instead of p tag

--- src/components/PlayerCard/PlayerCard.js
+++ src/components/PlayerCard/PlayerCard.js
@@ -1,15 +1,31 @@
 import React from 'react'
 
+import './PlayerCard.css'
+
 export default function PlayerCard({ player }) {
     return (
-        <li key={player.id || player.name}>
-            <img src={player.img} alt={player.name}/>
-            <p>{Math.ceil((Number(player.att) + Number(player.def) + Number(player.spd)) / 3)} AVG.</p>
-            <p>Name: {player.name}</p>
-            <p>Pos: {player.pos}</p>
-            <p>ATT: {player.att}</p>
-            <p>DEF: {player.def}</p>
-            <p>SPD: {player.spd}</p>
+        <li className="player-card" key={player.id || player.name}>
+            <img className="player-img" src={player.img} alt={player.name}/>
+            <ul>
+                <li>
+                    {Math.ceil((Number(player.att) + Number(player.def) + Number(player.spd)) / 3)} AVG.
+                </li>
+                <li>
+                    Name: {player.name}
+                </li>
+                <li>
+                    Pos: {player.pos}
+                </li>
+                <li>
+                    ATT: {player.att}
+                </li>
+                <li>
+                    DEF: {player.def}
+                </li>
+                <li>
+                    SPD: {player.spd}
+                </li>
+            </ul>
         </li>
     )
 }
