a071fe2f45f7d909d47b335cbe906503ca7c43e7
Erick Martinez <erick.martinez2013@gmail.com>
2019-06-21 19:20:11 -0500

    Styling for the login form component

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -5,6 +5,9 @@ import AuthApiService from '../../services/auth-api-service'
 
 import PlayersContext from '../../context/PlayersContext'
 
+import './LoginForm.css'
+import Logo from '../../imgs/ajax-loader.gif'
+
 export default class LoginPage extends React.Component {
     static contextType = PlayersContext;
 
@@ -12,11 +15,14 @@ export default class LoginPage extends React.Component {
         onLoginSuccess: () => {}
     }
     
-    state = { error: null }
+    state = { 
+        error: null,
+        loader: false
+    }
     
     handleSubmitJwtAuth = ev => {
         ev.preventDefault()
-        this.setState({ error: false })
+        this.setState({ error: false, loader: true })
         const { username, password } = ev.target
     
         AuthApiService.postLogin({
@@ -31,20 +37,20 @@ export default class LoginPage extends React.Component {
             this.context.signedInUpdate(true)
         })
         .catch(res => {
-            this.setState({ error: res.error })
+            this.setState({ error: res.error, loader: false })
         })
     }
       
     render() {
         const { error } = this.state
         return (
-            <form onSubmit={this.handleSubmitJwtAuth}>
+            <form className="login-form" onSubmit={this.handleSubmitJwtAuth}>
                 <fieldset>
-                    <legend>LOGIN</legend>
+                    <legend className="login-legend">LOGIN</legend>
 
-                    <input placeholder="username" name="username" required />
-                    <input type="password" placeholder="password" name="password" required />
-                    <button>Sign-In</button>
+                    <input className="login username" placeholder="username" name="username" required />
+                    <input className="login password" type="password" placeholder="password" name="password" required />
+                    <button className="submit-offline">{!this.state.loader ? "Sign-In" : <img src={Logo} alt="loading" />}</button>
                     
                     <div role='alert'>
                         {!!error && <p className='red'>{error}</p>}
