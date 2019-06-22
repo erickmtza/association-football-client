4b325196336ccdfa8d8853326d749c82478a5f1c
Erick Martinez <erick.martinez2013@gmail.com>
2019-06-22 14:34:41 -0500

    Added styling to not found page

--- src/routes/NotFoundPage/NotFoundPage.js
+++ src/routes/NotFoundPage/NotFoundPage.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './NotFoundPage.css'
 
 export default function NotFoundPage() {
     return (
-        <section>
-            <h2>404 - Page not found</h2>
-            <p>Try going back to your previous page.</p>
+        <section className="not-found">
+            <h1 className="page-not-found">404 - Page not found</h1>
+            <p>Try going back to your previous page or click <Link to="/" className="home-text">HOME</Link></p>
         </section>
     )
 }
